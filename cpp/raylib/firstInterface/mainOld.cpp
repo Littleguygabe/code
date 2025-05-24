5bf20bf37dcfa6a57c615df055e1c1f3b3027967
Gabriel Bridger <106691358+Littleguygabe@users.noreply.github.com>
2025-05-24 18:06:14 +0100

    removed main.cpp to eliminate outdated particle simulation code and streamline the project structure

--- cpp/raylib/firstInterface/mainOld.cpp
+++ cpp/raylib/firstInterface/mainOld.cpp
@@ -1,11 +1,12 @@
 #include "raylib.h"
-#include <vector>
+#include "raymath.h"
+#include <iostream>
 
 
-const int N_PARTICLES = 10000;
+const int N_PARTICLES = 50000;
 const int SCREEN_WIDTH = 1000;
 const int SCREEN_HEIGHT = 800;
-
+const float PARTICLE_RADIUS = 3.0f;
 
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "2D Particle Example");
@@ -14,7 +15,7 @@ int main() {
     std::vector<Vector2> positions(N_PARTICLES);
     std::vector<Vector2> velocities(N_PARTICLES);
 
-    for (int i = 0; i < MAX_PARTICLES; ++i) {
+    for (int i = 0; i < N_PARTICLES; ++i) {
         positions[i] = {
             static_cast<float>(rand() % SCREEN_WIDTH),
             static_cast<float>(rand() % SCREEN_HEIGHT)
@@ -28,10 +29,26 @@ int main() {
 
     while (!WindowShouldClose()) {
 
+        float dt = GetFrameTime();
+
+        //update the particle positions
+        for (int i = 0;i<N_PARTICLES;i++){
+            positions[i] = Vector2Add(positions[i],Vector2Scale(velocities[i],dt));
+            if (positions[i].x<0 || positions[i].x>SCREEN_WIDTH){
+                velocities[i].x*=-1;
+            }
+            if (positions[i].y<0 || positions[i].y>SCREEN_WIDTH){
+                velocities[i].y*=-1;
+            }
+        }       
+
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawCircleV(particlePos, particleRadius, RED);
+        for (int i = 0;i<N_PARTICLES;i++){
+            DrawCircleV(positions[i],PARTICLE_RADIUS,DARKGRAY);
+        }
 
         EndDrawing();
     }
