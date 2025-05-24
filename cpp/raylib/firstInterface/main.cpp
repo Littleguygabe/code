8972d5c9fee0fcf3d82974e670d540c667540db8
Gabriel Bridger <106691358+Littleguygabe@users.noreply.github.com>
2025-05-24 09:48:05 +0100

    added a Particle class in main.cpp to implement basic particle movement and updated the window title to reflect a 2D particle example, enhancing the graphical interface and functionality

--- cpp/raylib/firstInterface/main.cpp
+++ cpp/raylib/firstInterface/main.cpp
@@ -1,13 +1,38 @@
 #include "raylib.h"
 
+
+class Particle{
+    private:
+        Vector2 startpos;
+        Vector2 velocity = (Vector2){1.0f,1.0f};        
+        float radius = 5.0f;
+
+    public:
+        Particle(){
+            this->startpos.x = GetRandomValue(0,GetScreenWidth());
+            this->startpos.y = GetRandomValue(0,GetScreenHeight());               
+        }
+
+        void moveParticle(){
+            
+        }
+}
 int main() {
-    InitWindow(800, 600, "Hello raylib!");
+    InitWindow(800, 600, "2D Particle Example");
     SetTargetFPS(60);
 
+    Vector2 particlePos = { 400, 300 }; // Center of the screen
+    float particleRadius = 5.0f;
+
     while (!WindowShouldClose()) {
+        // Example: move the particle slightly to the right each frame
+        particlePos.x += 1.0f;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Welcome to raylib on macOS!", 190, 280, 20, LIGHTGRAY);
+
+        DrawCircleV(particlePos, particleRadius, RED);
+
         EndDrawing();
     }
 
