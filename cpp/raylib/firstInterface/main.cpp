07acbb3337fbd97d950414b7e66e3fdce864342f
Gabriel Bridger <106691358+Littleguygabe@users.noreply.github.com>
2025-05-24 09:16:28 +0100

    implemented a basic raylib window in main.cpp, replacing outdated interface code with a functional graphical interface that displays a welcome message, enhancing the project's usability

--- cpp/raylib/firstInterface/main.cpp
+++ cpp/raylib/firstInterface/main.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
+#include "raylib.h"
 
-int main(){
+int main() {
+    InitWindow(800, 600, "Hello raylib!");
+    SetTargetFPS(60);
 
-    
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Welcome to raylib on macOS!", 190, 280, 20, LIGHTGRAY);
+        EndDrawing();
+    }
 
+    CloseWindow();
     return 0;
 }
