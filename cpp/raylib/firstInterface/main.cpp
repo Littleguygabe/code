caf9aab21832f7110d6ba397df82c5fe40766c30
Gabriel Bridger <106691358+Littleguygabe@users.noreply.github.com>
2025-05-24 13:04:53 +0100

    refactored main.cpp to improve particle simulation by defining constants for screen dimensions and particle count, increased frame rate to 120 FPS, and initialized vectors for particle positions and velocities, enhancing performance and visual experience

--- cpp/raylib/firstInterface/main.cpp
+++ cpp/raylib/firstInterface/main.cpp
@@ -1,32 +1,23 @@
 #include "raylib.h"
+#include <vector>
 
 
-class Particle{
-    private:
-        Vector2 startpos;
-        Vector2 velocity = (Vector2){1.0f,1.0f};        
-        float radius = 5.0f;
+const int N_PARTICLES = 10000;
+const int SCREEN_WDITH = 1000;
+const int SCREEN_HEIGHT = 800;
 
-    public:
-        Particle(){
-            this->startpos.x = GetRandomValue(0,GetScreenWidth());
-            this->startpos.y = GetRandomValue(0,GetScreenHeight());               
-        }
 
-        void moveParticle(){
-            
-        }
-}
 int main() {
-    InitWindow(800, 600, "2D Particle Example");
-    SetTargetFPS(60);
+    InitWindow(SCREEN_WDITH, SCREEN_HEIGHT, "2D Particle Example");
+    SetTargetFPS(120);
 
-    Vector2 particlePos = { 400, 300 }; // Center of the screen
-    float particleRadius = 5.0f;
+    std::vector<Vector2> positions(N_PARTICLES);
+    std::vector<Vector2> velocities(N_PARTICLES);
+
+
+    for (int i = 0;i<positions)
 
     while (!WindowShouldClose()) {
-        // Example: move the particle slightly to the right each frame
-        particlePos.x += 1.0f;
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
