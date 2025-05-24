0248769befc455dd6b13e074a97ab94e54d146d0
Gabriel Bridger <106691358+Littleguygabe@users.noreply.github.com>
2025-05-24 13:08:01 +0100

    fixed typo in SCREEN_WIDTH constant and added initialization for particle positions and velocities in main.cpp, improving the particle simulation setup

--- cpp/raylib/firstInterface/main.cpp
+++ cpp/raylib/firstInterface/main.cpp
@@ -3,19 +3,28 @@
 
 
 const int N_PARTICLES = 10000;
-const int SCREEN_WDITH = 1000;
+const int SCREEN_WIDTH = 1000;
 const int SCREEN_HEIGHT = 800;
 
 
 int main() {
-    InitWindow(SCREEN_WDITH, SCREEN_HEIGHT, "2D Particle Example");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "2D Particle Example");
     SetTargetFPS(120);
 
     std::vector<Vector2> positions(N_PARTICLES);
     std::vector<Vector2> velocities(N_PARTICLES);
 
+    for (int i = 0; i < MAX_PARTICLES; ++i) {
+        positions[i] = {
+            static_cast<float>(rand() % SCREEN_WIDTH),
+            static_cast<float>(rand() % SCREEN_HEIGHT)
+        };
 
-    for (int i = 0;i<positions)
+        velocities[i] = {
+            static_cast<float>((rand() % 200) - 100),  // [-100, 100]
+            static_cast<float>((rand() % 200) - 100)
+        };
+    }
 
     while (!WindowShouldClose()) {
 
