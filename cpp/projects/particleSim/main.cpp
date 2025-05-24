8ac3f97c89451a297286dbe1cf26831e5d7ff503
Gabriel Bridger <106691358+Littleguygabe@users.noreply.github.com>
2025-05-24 18:14:08 +0100

    added Vect2D struct and Particle class in main.cpp to define particle properties, enhancing the simulation's structure and functionality

--- cpp/projects/particleSim/main.cpp
+++ cpp/projects/particleSim/main.cpp
@@ -6,4 +6,17 @@ const int SCREEN_WIDTH = 1000;
 const int SCREEN_HEIGHT = 800;
 const float PARTICLE_RADIUS = 3.0f;
 
+struct Vect2D {
+    float x;
+    float y;
+};
 
+class Particle{
+    public:
+        Vect2D position;
+        Vect2D velocity;
+    
+        Particle(){
+
+        }
+}
