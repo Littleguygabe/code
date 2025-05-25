63c33bd85d9257a15a23839f7f44a18ccbbd2938
Gabriel Bridger <106691358+Littleguygabe@users.noreply.github.com>
2025-05-25 14:59:08 +0100

    Removed the CC GENERAL.csv dataset and added data loading and cleaning functionality in main.py, including dropping missing values and the CUST_ID column.

--- python/comp1008/creditCardClustering/main.py
+++ python/comp1008/creditCardClustering/main.py
@@ -2,3 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
+data = pd.read_csv('creditCardClustering/creditCardData.csv')
+
+cleanedData = data.dropna()
+cleanedData = cleanedData.drop('CUST_ID',axis=1)
+
+print(cleanedData)
