6ba4a40c2cc7ae3cdb9645c7a5f0341cb508ad07
Gabriel Bridger <106691358+Littleguygabe@users.noreply.github.com>
2025-05-25 11:18:31 +0100

    added spatial grid structure and hash function for particle management in main.cpp, enhancing simulation efficiency and organization

--- cpp/projects/particleSim/main.cpp
+++ cpp/projects/particleSim/main.cpp
@@ -1,10 +1,24 @@
 #include "raylib.h"
 #include "raymath.h"
+#include <functional>
+#include <iostream>
+#include <unordered_map>
+#include <utility>
 
 const int N_PARTICLES = 100;
 const int SCREEN_WIDTH = 1000;
 const int SCREEN_HEIGHT = 800;
 const float PARTICLE_RADIUS = 3.0f;
+
+namespace std {
+    template <>
+    struct hash<std::pair<int, int>> {
+        size_t operator()(const std::pair<int, int>& p) const {
+            return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+        }
+    };
+}
+
 
 struct Vect2D {
     float x;
@@ -14,5 +28,19 @@ struct Vect2D {
 struct Particle {
     Vect2D position;
     Vect2D velocity;
-    float radius = 3.0f;
+    float radius = PARTICLE_RADIUS;
 };
+
+int main(){
+    using CellPos = std::pair<int, int>;
+    using SpatialGrid = std::unordered_map<CellPos, std::vector<Particle*>>;
+
+    
+
+
+
+    std::cout<<"system running..."<<std::endl;
+
+
+    return 0;
+}
