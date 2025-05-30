e150309dddcb1f73d3fbb009c1bd76eefa0a0f4b
Gabriel Bridger <106691358+Littleguygabe@users.noreply.github.com>
2025-05-30 09:45:44 +0100

    Remove minimax.py, eliminating the binary tree structure, node creation, and tree visualization functions. This cleanup reduces code complexity and removes unused functionality.

---
+++ python/ai/treeStructure.py
@@ -0,0 +1,53 @@
+import numpy as np
+import random
+from dataclasses import dataclass
+from typing import Optional
+
+
+class Tree:
+    @dataclass
+    class Node:
+        val:int
+        left:Optional['Tree.Node'] = None
+        right:Optional['Tree.Node'] = None
+
+
+    def __init__(self,depth) -> None:
+        self.depth = depth
+        self.root = self.createTree()
+
+    def createTree(self):
+        def constructor(count):
+            if count == 0:
+                return None
+            
+            node = self.Node(random.randint(1,9))
+            node.left = constructor(count-1)
+            node.right = constructor(count-1)
+
+            return node 
+
+        root = constructor(self.depth)
+
+        return root
+
+    def printTree(self,node=None, level=0):
+        if node is None:
+            node = self.root
+        print('  ' * level + f'Node(val={node.val})')
+
+        if node.left:
+            self.printTree(node.left, level + 1)
+        
+        if node.right:
+            self.printTree(node.right, level + 1)
+
+
+def main():
+    testTree = Tree(3)
+    testTree.printTree()
+
+
+
+if __name__ == '__main__':
+    main()
