514210badb572e088fcfaa313059fd8472cb9087
Gabriel Bridger <106691358+Littleguygabe@users.noreply.github.com>
2025-06-09 19:34:53 +0100

    Add BigramLanguageModel class to bigramLanguageModel.py for language modeling
    Implemented the BigramLanguageModel class with an embedding layer and a forward method for processing input indices. Added a sample usage of the model with a batch of training data, enhancing the project's capabilities for language modeling tasks.

--- python/projects/handmadeGPT/bigramLanguageModel.py
+++ python/projects/handmadeGPT/bigramLanguageModel.py
@@ -3,3 +3,24 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 torch.manual_seed(1337)
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocabSize) -> None:
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocabSize,vocabSize)
+
+    def forward(self,idx,targets):
+        logits = self.token_embedding_table(idx)
+
+        return logits
+    
+
+m = BigramLanguageModel(vocabSize)
+
+
+xb,yb = getBatch('train')
+
+out = m(xb,yb)
+print(out.shape)
+
+
